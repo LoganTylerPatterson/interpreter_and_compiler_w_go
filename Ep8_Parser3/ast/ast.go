05f380f4b199b8006a366481324fc559c1c12717
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2024-04-13 17:31:51 -0700

    Parsing prefix Expressions BABABAY

--- Ep8_Parser3/ast/ast.go
+++ Ep8_Parser3/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "interpreter/token"
+import (
+	"bytes"
+	"interpreter/token"
+)
 
 type Node interface {
 	TokenLexeme() string
+	String() string
 }
 
 type Statement interface {
@@ -28,7 +32,17 @@ func (p *Program) TokenLexeme() string {
 	}
 }
 
-//	LET STATEMENTS //
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// LET STATEMENTS //
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -37,8 +51,23 @@ type LetStatement struct {
 
 func (ls *LetStatement) TokenLexeme() string { return ls.Token.Lexeme }
 func (ls *LetStatement) statementNode()      {}
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
 
-//	IDs //
+	out.WriteString(ls.TokenLexeme() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// IDs //
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -46,6 +75,9 @@ type Identifier struct {
 
 func (id *Identifier) TokenLexeme() string { return id.Token.Lexeme }
 func (id *Identifier) expressionNode()     {}
+func (id *Identifier) String() string {
+	return id.Value
+}
 
 /** 	RETURN STATEMENTS		**/
 type ReturnStatement struct {
@@ -57,3 +89,61 @@ type ReturnStatement struct {
 
 func (rt *ReturnStatement) TokenLexeme() string { return rt.Token.Lexeme }
 func (rt *ReturnStatement) statementNode()      {}
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLexeme() + " ")
+
+	if rs.Value != nil {
+		out.WriteString(rs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+/**  	EXPRESSION STATEMENTS 	**/
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) TokenLexeme() string { return es.Token.Lexeme }
+func (es *ExpressionStatement) statementNode()      {}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+/** INTEGER LITERAL EXPRESSIONS **/
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) TokenLexeme() string { return il.Token.Lexeme }
+func (il *IntegerLiteral) expressionNode()     {}
+func (il *IntegerLiteral) String() string      { return il.Token.Lexeme }
+
+/** PREFIX EXPRESSIONS **/
+type PrefixExpression struct {
+	Token token.Token
+	Op    string
+	Value Expression
+}
+
+func (pe *PrefixExpression) expressionNode()     {}
+func (pe *PrefixExpression) TokenLexeme() string { return pe.Token.Lexeme }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Op)
+	out.WriteString(pe.Value.String())
+	out.WriteString(")")
+
+	return out.String()
+}
--- Ep8_Parser3/parser/parser.go
+++ Ep8_Parser3/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"interpreter/ast"
 	"interpreter/lexer"
 	"interpreter/token"
+	"strconv"
 )
 
 // we need tokens provided by {lexer}
@@ -19,6 +20,9 @@ type Parser struct {
 	nextToken    token.Token
 	//list of errors to return after parsing
 	errors []string
+	//hashmap of infix and prefix operators
+	prefixParseFuncs map[token.TokenType]prefixParse
+	infixParseFuncs  map[token.TokenType]infixParse
 }
 
 func New(lexer *lexer.Lexer) *Parser {
@@ -27,16 +31,53 @@ func New(lexer *lexer.Lexer) *Parser {
 		errors: []string{},
 	}
 
+	parser.prefixParseFuncs = make(map[token.TokenType]prefixParse)
+	parser.addPrefixToken(token.ID, parser.parseIdentifier)
+	parser.addPrefixToken(token.DIGIT, parser.parseIntegerLiteral)
+	parser.addPrefixToken(token.MINUS, parser.parsePrefixExpression)
+	parser.addPrefixToken(token.EXCLAM, parser.parsePrefixExpression)
+
 	parser.getToken()
 	parser.getToken()
 
 	return parser
 }
 
+func (parser *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: parser.currentToken, Value: parser.currentToken.Lexeme}
+}
+
+func (parser *Parser) parseIntegerLiteral() ast.Expression {
+	il := &ast.IntegerLiteral{Token: parser.currentToken}
+
+	value, err := strconv.ParseInt(parser.currentToken.Lexeme, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", parser.currentToken.Lexeme)
+		parser.errors = append(parser.errors, msg)
+		return nil
+	}
+
+	il.Value = value
+	return il
+}
+
 // Get the next token from our lexer
 func (parser *Parser) getToken() {
 	parser.currentToken = parser.nextToken
 	parser.nextToken = parser.lexer.GetToken()
+}
+
+type (
+	prefixParse func() ast.Expression
+	infixParse  func(ast.Expression) ast.Expression
+)
+
+func (parser *Parser) addPrefixToken(tokenType token.TokenType, fnc prefixParse) {
+	parser.prefixParseFuncs[tokenType] = fnc
+}
+
+func (parser *Parser) addInfixToken(tokenType token.TokenType, fnc infixParse) {
+	parser.infixParseFuncs[tokenType] = fnc
 }
 
 // PROG = STMT_LIST
@@ -57,15 +98,64 @@ func (parser *Parser) ParseProgram() *ast.Program {
 	return program
 }
 
-// STMT = LET_STMT
 func (parser *Parser) parseStatement() ast.Statement {
 	switch parser.currentToken.Type {
 	case token.LET:
 		return parser.parseLetStatement()
+	case token.RETURN:
+		return parser.parseReturnStatement()
 	default:
-		return nil
+		return parser.parseExpressionStatement()
 	}
 }
+
+func (parser *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: parser.currentToken}
+
+	stmt.Expression = parser.parseExpression(NONE)
+
+	if parser.nextTokenIs(token.SEMICOLON) {
+		parser.getToken()
+	}
+
+	return stmt
+}
+
+func (parser *Parser) parseExpression(precedence int) ast.Expression {
+	prefixFunc := parser.prefixParseFuncs[parser.currentToken.Type]
+
+	if prefixFunc == nil {
+		return nil
+	}
+
+	leftExpr := prefixFunc()
+
+	return leftExpr
+}
+
+func (parser *Parser) parsePrefixExpression() ast.Expression {
+	pe := &ast.PrefixExpression{
+		Token: parser.currentToken,
+		Op:    parser.currentToken.Lexeme,
+	}
+
+	parser.getToken()
+
+	pe.Value = parser.parseExpression(PREFIX)
+
+	return pe
+}
+
+const (
+	_ int = iota
+	NONE
+	EQUALS        // ==
+	LESSERGREATER // < or >
+	SUM           // +
+	MULT          // *
+	PREFIX        // !<condition>
+	CALL          //function call
+)
 
 // LET_STMT = LET ID EQ EXPRESSION
 func (parser *Parser) parseLetStatement() *ast.LetStatement {
